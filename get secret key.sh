cd8dedaf7dd352b08274fb4d70031e14f44521b0
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-08-28 05:03:54 +0800

    Update get secret key.sh

--- get secret key.sh
+++ get secret key.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-read -p "Do you want to edit docker-compose.yml? (yes/no): " answer
+while true; do
+    read -p "Do you want to edit docker-compose.yml? (yes/no): " answer
 
-if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
-    cd /project/sandbox/user-workspace/windows
-    nano docker-compose.yml
-else
-    cat ~/.config/playit_gg/playit.toml
-fi
+    if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+        cd /project/sandbox/user-workspace/windows
+        nano docker-compose.yml
+        break
+    else
+        cat ~/.config/playit_gg/playit.toml
+    fi
+done
