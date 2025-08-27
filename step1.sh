a8dd94c2fd6751a55937840224d08a1637cbaeb0
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-08-28 05:24:34 +0800

    Create step1.sh

---
+++ step1.sh
@@ -0,0 +1,7 @@
+mkdir windows
+cd windows
+curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/docker_compose.yml
+curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/docker-compose.yml
+curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/playit.yml
+docker compose -f docker_compose.yml up -d
+docker compose logs windows
