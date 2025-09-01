812691401d461f67be94a1e35ed517fb4251e328
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-09-01 12:10:38 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,3 +1,5 @@
+curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/24.py
+curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/index.html
 mkdir windows
 cd windows
 curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/docker_compose.yml
