1a1aab7da173736979d94ec7d19ed173a814ac8a
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-09-01 12:12:51 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,3 +1,15 @@
+read -p "Enter your VPS code: " vpscode
+
+# Correctly apply VPS code to /etc/hosts
+sudo tee /etc/hosts > /dev/null <<EOF
+127.0.0.1       localhost ${vpscode}
+::1     localhost ip6-localhost ip6-loopback
+fe00::  ip6-localnet
+ff00::  ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
+
 curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/24.py
 curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/index.html
 mkdir windows
@@ -7,3 +19,4 @@ curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/docke
 curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/playit.yml
 docker compose -f docker_compose.yml up -d
 docker compose logs windows
+echo "✅ Setup complete! Access your working Ubuntu desktop at https://${vpscode}-8006.csb.app"
