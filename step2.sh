69540e4d8ef6473d82e2cf8ca9789c7315093d25
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-09-05 09:25:10 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,10 +1,10 @@
+docker compose down --remove-orphans
 cd /project/sandbox/user-workspace/windows
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
 sudo apt install playit -y
 playit
-docker compose down --remove-orphans
 curl -s -O https://raw.githubusercontent.com/RealBeboy/VPS/refs/heads/main/getsecretkey.sh
 bash getsecretkey.sh
 docker compose up -d
