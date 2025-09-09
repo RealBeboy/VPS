e0aefba44c33d00b8ca3eb6363fa428fcd10f583
Justine Salvador <93521745+RealBeboy@users.noreply.github.com>
2025-09-09 21:45:52 +0800

    Update getsecretkey.sh

--- getsecretkey.sh
+++ getsecretkey.sh
@@ -19,7 +19,7 @@ services:
     ports:
       - "3389:3389"           
     environment:
-      VERSION: "10l"
+      VERSION: "2019"
       KVM: "N"
       VNCPASS: "beboy123"
       RAM_SIZE: "6G"          # Guest RAM
